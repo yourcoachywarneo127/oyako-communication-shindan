363661cbb5cd24e168547fca59ddf48df79493b5
yourcoachywarneo127 <yourcoach.yw.arneo127@gmail.com>
2026-08-17 14:34:00 +0900

    Update ProgressBar.tsx

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 interface Props {
   current: number;
   total: number;
 }
 
 export default function ProgressBar({ current, total }: Props) {
-  const percentage = Math.round((current / total) * 100);
+  const percentage = total > 0 ? Math.round((current / total) * 100) : 0;
 
   return (
     
