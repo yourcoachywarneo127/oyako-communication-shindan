129c1f94848ce944526484503a86d0bafeb3215c
yourcoachywarneo127 <yourcoach.yw.arneo127@gmail.com>
2026-08-17 16:33:36 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // GitHub Pages用のパス設定
+  basePath: process.env.NODE_ENV === 'production' ? '/oyako-communication-shindan' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/oyako-communication-shindan/' : '',
 };
 
 module.exports = nextConfig;
