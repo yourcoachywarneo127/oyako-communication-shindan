a75afd2c91694fee80010cb0444690d1874a35d6
yourcoachywarneo127 <yourcoach.yw.arneo127@gmail.com>
2026-08-17 14:12:22 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: '親子のコミュニケーション診断',
+  description: '親子のコミュニケーション診断アプリ',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    
+      {children}
+    
+  )
+}
