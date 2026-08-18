01373e75576b06c8e816a8fde2f9909ef1be9468
yourcoachywarneo127 <yourcoach.yw.arneo127@gmail.com>
2026-08-18 23:47:27 +0900

    Update config.ts

--- data/config.ts
+++ data/config.ts
@@ -1,4 +1,4 @@
 // 無料勉強会CTA用URL（後から自由に変更可能）
-export const CTA_URL = "https://example.com/free-seminar";
+export const CTA_URL = "https://line.me/R/oaMessage/${'506hokix'}/?${encodeURIComponent('勉強会希望')}`";
 
 export const APP_NAME = "親子コミュニケーションタイプ診断";
